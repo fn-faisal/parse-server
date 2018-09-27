bf63d1b7f9dc80d64dd27539c5cc860d8ff31498
Ricardo Paiva <ricardo@ricardopaiva.com>
2018-09-27 10:34:58 -0300

    Fix ignore badge update error

--- src/Controllers/PushController.js
+++ src/Controllers/PushController.js
@@ -73,13 +73,9 @@ export class PushController {
       return pushStatus.setInitial(body, where);
     }).then(() => {
       onPushStatusSaved(pushStatus.objectId);
-      return badgeUpdate();
-    }).catch(err => {
-      // add this to ignore badge update errors as default
-      if (config.stopOnBadgeUpdateError) throw err
-      logger.info(`Badge update error will be ignored for push status ${pushStatus.objectId}`)
-      logger.info(err && err.stack && err.stack.toString() || err.toString())
-      return Promise.resolve()
+      if (config.stopOnBadgeUpdateError) return badgeUpdate();
+      logger.info(`Badge update error will be ignored for push status ${pushStatus.objectId}`);
+      return Promise.resolve();
     }).then(() => {
       // Update audience lastUsed and timesUsed
       if (body.audience_id) {
